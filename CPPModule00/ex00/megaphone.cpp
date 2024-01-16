c99ba262d1b773d176e7861b0c0eb4653ec0cbd5
Erwin Kuchel <ekuchel@c2r14s8.42wolfsburg.de>
2024-01-16 16:57:59 +0100

    ex00_done

--- CPPModule00/ex00/megaphone.cpp
+++ CPPModule00/ex00/megaphone.cpp
@@ -10,16 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cctype>
 #include <iostream>
 
 int	main(int argc, char **argv)
 {
+	int	i = 1;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		
-	}
-
+		while (i < argc)
+		{
+			int	j = 0;
+			while (argv[i][j]) 
+			{
+				std::cout << (char) toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+			// if (i != argc)
+			// 	std::cout << ' ' ;
+		}	
+		std::cout << std::endl;
+	}	
 	return (0);
 }
