e9ed60c766249c565aab947ae18e0cc92ccadda6
Erwin Kuchel <ekuchel@c2r14s8.42wolfsburg.de>
2024-02-02 17:42:08 +0100

    phonebook-practice

--- CPPModule00/ex01/Phonebook.class.cpp
+++ CPPModule00/ex01/Phonebook.class.cpp
@@ -6,7 +6,51 @@
 /*   By: ekuchel <ekuchel@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 17:48:07 by ekuchel           #+#    #+#             */
-/*   Updated: 2024/02/01 17:48:23 by ekuchel          ###   ########.fr       */
+/*   Updated: 2024/02/02 17:30:38 by ekuchel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Phonebook.class.hpp"
+
+std::string Contact::getName(std::string column)
+{
+    if (column.compare("firstName") == 0)
+        return this->_firstName;
+    else if (column.compare("lastName") == 0)
+
+        return this->_lastName;
+    else if (column.compare("nickName") == 0)
+        return this->_nickName;
+    else if (column.compare("phone") == 0)
+        return this->_phone;
+    else if (column.compare("darkestSecret") == 0)
+        return this->_darkestSecret;    
+}
+
+
+bool    Contact::setName(std::string input, std::string column)
+{
+    std::string trunc_input;
+
+    if (input.length() >= 10)
+    if (column.compare("firstName") == 0)
+        this->_firstName = input;
+    else if (column.compare("lastName") == 0)
+        this->_lastName = input;
+    else if (column.compare("nickName") == 0)
+        this->_nickName = input;
+    else if (column.compare("phone") == 0)
+        this->_phone = input;
+    else if (column.compare("darkestSecret") == 0)
+        this->_darkestSecret = input;    
+    else
+        return false;
+    return true;
+}
+
+bool    PhoneBook::addContact(Contact contact, int index)
+{
+
+    return true;
+}
--- CPPModule00/ex01/Phonebook.class.hpp
+++ CPPModule00/ex01/Phonebook.class.hpp
@@ -1,27 +1,38 @@
 #ifndef PHONEBOOK_CLASS_H
 # define PHONEBOOK_CLASS_H
 
+# include <string>
+
 class Contact
 {
-	std::string _firstname;
-	std::string _lastname;
-	std::string _nickname;
+public:
+    bool setName(std::string input, std::string column);
+    std::string getName(std::string column);
+
+private:
+	std::string _firstName;
+	std::string _lastName;
+	std::string _nickName;
 	std::string _phone;
+    std::string _darkestSecret
 };
 
 class PhoneBook
 {
 public:
-
 	PhoneBook(void);
 	~PhoneBook(void);
 
-	void add_contact(int i);
-	void search_contact(std::string contact);
-	void exit_program(void) const;
-	Contact contact[8];
+	bool addContact(Contact contact, int index);
+	bool searchContact(Contact *contact, std::string name);
+	void exitProgram(void) const;
 
+private:
+	Contact _contact[8];
 };
 
 
 #endif
+
+//functions in classes are called "methods"
+//variables in classes are called "members"
