6f88acae11e31d7268478ae4c3fa7f6dac27a0e8
Erwin Kuchel <ekuchel@c2r14s8.42wolfsburg.de>
2024-02-09 18:40:34 +0100

    Module00_done

--- CPPModule00/ex02/Account.cpp
+++ CPPModule00/ex02/Account.cpp
@@ -6,40 +6,45 @@
 /*   By: ekuchel <ekuchel@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 12:56:23 by ekuchel           #+#    #+#             */
-/*   Updated: 2024/02/07 18:23:42 by ekuchel          ###   ########.fr       */
+/*   Updated: 2024/02/09 18:38:48 by ekuchel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 #include <ctime>
+#include <iomanip>
 
 #define RESET   "\033[0m"
 #define MAGENTA "\033[35m"
 
-Account::Account(int initial_deposit) : _amount(initial_deposit)
-{
-    _accountIndex = _nbAccounts;
-    _nbDeposits = 0;
-    _nbWithdrawals = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
-	_totalAmount += initial_deposit;
-	_totalNbDeposits++;
-	_displayTimestamp();
-	std::cout	<< "index:" << MAGENTA << _accountIndex << RESET
-				<< ";amount:" << MAGENTA << _totalAmount << RESET
-				<< ";created" << std::endl;
-
-    _nbAccounts++;
-}
 
 Account::Account(void)
 {
-    _accountIndex = _nbAccounts;
-    _amount = 0;
-    _nbDeposits = 0;
-    _nbWithdrawals = 0;
+	_accountIndex = _nbAccounts;
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
 
+	_nbAccounts++;
+}
+
+Account::Account(int initial_deposit) : _amount(initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex
+				<< ";amount:" << _amount
+				<< ";created" << std::endl;
     _nbAccounts++;
 }
 
@@ -47,8 +52,8 @@ Account::~Account(void)
 {
 	_nbAccounts--;
 	_displayTimestamp();
-	std::cout	<< "index:" << MAGENTA << _accountIndex << RESET
-				<< ";amount:" << MAGENTA << _totalAmount << RESET
+	std::cout	<< "index:" << _accountIndex
+				<< ";amount:" << _amount
 				<< ";closed" << std::endl;
 }
 
@@ -75,32 +80,45 @@ int	Account::getNbWithdrawals(void)
 void	Account::displayAccountsInfos(void)
 {
 	_displayTimestamp();
-	std::cout << "accounts:" << MAGENTA << _nbAccounts << RESET
-			<< "total:" << MAGENTA << _totalAmount << RESET
-			<< ";deposits:" << MAGENTA << _totalNbDeposits << RESET
-			<< "withdrawals" << MAGENTA << _totalNbWithdrawals << RESET
+	std::cout << "accounts:" << _nbAccounts
+			<< ";total:" << _totalAmount
+			<< ";deposits:" << _totalNbDeposits
+			<< ";withdrawals:" << _totalNbWithdrawals
 			<< std::endl;
 }
 
 void	Account::makeDeposit(int deposit)
 {
-	_displayTimestamp();
+	if (deposit > 0)
+	{
+		_amount += deposit;
+		_totalAmount += deposit;
+		_nbDeposits++;;
+		_totalNbDeposits++;
+		_displayTimestamp();
+		std::cout << "index:" << _accountIndex
+				<< ";p_amount:" << _amount - deposit
+				<< ";deposit:" << deposit
+				<< ";amount:" << _amount
+				<< ";nb_deposits:" << _nbDeposits
+				<< std::endl;
+	}
 }
 
 bool	Account::makeWithdrawal(int withdrawal)
 {
 	_displayTimestamp();
-	std::cout	<< "index:" << MAGENTA << _accountIndex << RESET
-				<< ";p_amount:" << MAGENTA << _amount << RESET;
-	if (withdrawal <= _amount)
+	std::cout	<< "index:" << _accountIndex
+				<< ";p_amount:" << _amount;
+	if (withdrawal <= _amount && withdrawal > 0)
 	{
 		_amount -= withdrawal;
 		_totalAmount -= withdrawal;
 		_nbWithdrawals++;
 		_totalNbWithdrawals++;
-		std::cout	<< ";withdrawal:" << MAGENTA << withdrawal << RESET
-					<< ";amount:" << MAGENTA << _amount << RESET
-					<< ";nb_withdrawals:" << MAGENTA << _nbWithdrawals << RESET << std::endl;
+		std::cout	<< ";withdrawal:" << withdrawal
+					<< ";amount:" << _amount
+					<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 		return true;
 	}
 	else
@@ -118,8 +136,22 @@ int	Account::checkAmount(void) const
 void	Account::displayStatus(void) const
 {
 	_displayTimestamp();
-		std::cout	<< "index:" << MAGENTA << _accountIndex << RESET
-					<< ";amount:" << MAGENTA << _amount << RESET
-					<< ";deposits:" << MAGENTA << _nbDeposits << RESET
-					<< ";withdrawals:" << MAGENTA << _nbWithdrawals << RESET << std::endl;
+		std::cout	<< "index:" << _accountIndex
+					<< ";amount:" << _amount
+					<< ";deposits:" << _nbDeposits
+					<< ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void	Account::_displayTimestamp( void )
+{
+	time_t now = time(0);
+	struct tm *ltime = localtime(&now);
+	std::cout	<< "[" << 1900 + ltime->tm_year
+				<< std::setfill ('0') << std::setw(2) << 1 + ltime->tm_mon
+				<< std::setfill ('0') << std::setw(2) << ltime->tm_mday
+				<< "_"
+				<< std::setfill ('0') << std::setw(2) << ltime->tm_hour
+				<< std::setfill ('0') << std::setw(2) << ltime->tm_min
+				<< std::setfill ('0') << std::setw(2) << ltime->tm_sec
+				<< "] ";
 }
