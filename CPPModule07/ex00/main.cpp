7a35aa278d68bea201eb8012b48d54bf6f100e32
Erwin Küchel <e.kuechel>
2024-08-15 16:58:06 +0200

    cpp07/ex00

--- CPPModule07/ex00/main.cpp
+++ CPPModule07/ex00/main.cpp
@@ -1,6 +1,42 @@
-
+#include <iostream>
+#include <string>
+#include "whatever.hpp"
 
 int main(void)
 {
+	int z = 1;
+	int y = 1;
+	int &x = max(z, y);
 
+	//swap(a, b);
+
+	std::cout << "z: " << &z << " y: " << &y << std::endl;
+	std::cout << "c: " << &x << std::endl;
+
+	std::cout << "\nstrings" << std::endl;
+
+	std::string f = "Hello";
+	std::string g = "Hellp";
+	std::string &j = max(f, g);
+
+	swap(f, g);
+
+	std::cout << "f: " << f << " g: " << g << std::endl;
+	std::cout << "j: " << j << std::endl;
+	std::cout << min(f, g) << std::endl;
+
+
+	std::cout << "\nTest input" << std::endl;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 }
