d309c38553b34507f5ce546f7db49d6da9fa09cd
Erwin Küchel <e.kuechel>
2024-08-20 19:48:48 +0200

    ex02

--- CPPModule07/ex02/Array.hpp
+++ CPPModule07/ex02/Array.hpp
@@ -1,22 +1,44 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
-#include <string>
+//#include <string>
+#include <exception>
+#include <stdexcept>
 
 template<typename T>
 class Array 
 {
 	private:
+		T *array;
+		unsigned int arr_size;
 		// Add your private members here
 
 	public:
-		Array(unsigned int n); // Creates an array of n elements initialized by default.
+		Array(unsigned int n) : arr_size(n) 
+		{
+			array = new T[n];
+		} // Creates an array of n elements initialized by default.
 
-		Array(void);//creates an empty array
+		Array() : array(0), arr_size(0) {} //creates an empty array
 		Array(const Array& instance);
-		Array &operator=(const Array& rhs);
-		~Array(void);
+		Array &operator=(const Array& rhs)
+		{
+			fort
+			array
+		}
+		~Array(void)
+		{
+			if(array)
+				delete[] array;
+		}
 
-
+		T& operator[](unsigned int idx)
+		{
+			if(idx < arr_size)
+				return array[idx];
+			else
+				throw std::out_of_range("Error: Out of bounds");
+		}
+		unsigned int size() const {return arr_size;}
 
 };
 
--- CPPModule07/ex02/main.cpp
+++ CPPModule07/ex02/main.cpp
@@ -1,14 +1,28 @@
+#include <exception>
 #include <iostream>
-//#include "iter.hpp"
+#include "Array.hpp"
 
 int main(void)
 {
+	try
+	{
+		Array<int> a(4);
+		//int a = 1;
+		//int b = 42;
 
-	//int a = 1;
-	//int b = 42;
 
-
-	int a[4] = {1, 2, 3, 4};
-	int* p = &a[2];
-	std::cout << p[2] << "\t" << p[-1] << 1[p] << (-1)[p] << '\n';
+		//int a[4] = {1, 2, 3, 4};
+		a[9] = 42;
+		std::cout << "idx [2]: " << a[2] << std::endl;
+		for(size_t i = 0; i < a.size(); i++)
+		{
+			a[i] = i * i * i;
+			std::cout << "idx [" << i << "] " << a[i] << std::endl;
+			//std::cout << p[2] << "\t" << p[-1] << 1[p] << (-1)[p] << '\n';
+		}
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
