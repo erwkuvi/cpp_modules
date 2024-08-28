9c94f49e1aabfc147071ee1a80059487106c9c97
Erwin Küchel <e.kuechel>
2024-08-28 13:24:24 +0200

    cpp08/ex02/main-update

--- CPPModule08/ex02/main.cpp
+++ CPPModule08/ex02/main.cpp
@@ -53,4 +53,55 @@ int main(void)
 	}
 	std::cout << "end" << std::endl;
 
+
+	std::cout << "\nRegular Container" << std::endl;
+	std::vector<int> test1;
+	test1.push_back(1);
+	test1.push_back(16);
+
+	std::cout << "idx: " << test1[1] << std::endl;
+	std::cout << "top: " << test1.back() << std::endl;
+	test1.pop_back();
+	std::cout << "test1 size: " << test1.size() << std::endl;
+	test1.push_back(12);
+	test1.push_back(216);
+
+	std::vector<int>::iterator it1 = test1.begin();
+	std::vector<int>::iterator ite1 = test1.end();
+
+	//test1.test1();
+	while(it1 != ite1)
+	{
+		std::cout << "*it1: " << *it1 << std::endl;
+		++it1;
+	}
+	std::cout << "\n" << std::endl;
+
+	std::vector<int> b1;
+	b1.push_back(9);
+	b1.push_back(916);
+
+	std::vector<int> a1(test1);
+	std::vector<int>::iterator ita1 = a1.begin();
+	std::vector<int>::iterator itea1 = a1.end();
+
+	//test1.test1();
+	while(ita1 != itea1)
+	{
+		std::cout << "*ita: " << *ita1 << std::endl;
+		++ita1;
+	}
+	std::cout << "there" << std::endl;
+
+	a1 = b1;
+	ita1 = a1.begin();
+	itea1 = a1.end();
+	std::cout << "\n" << std::endl;
+	while(ita1 != itea1)
+	{
+		std::cout << "*itb1: " << *ita1 << std::endl;
+		++ita1;
+	}
+	std::cout << "end" << std::endl;
+
 }
